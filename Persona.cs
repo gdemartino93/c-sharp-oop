36676bcebb4e0a3954610019f3b7b60f53b823ed
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-03-26 12:19:10 +0200

    creating class

---
+++ Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Persona
+    {
+        private string nome;
+        private string cognome;
+        private string dataDiNascita;
+        private string citta;
+
+
+        public Persona(string nome, string cognome, string dataDiNascita, string citta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.dataDiNascita = dataDiNascita;
+            this.citta = citta;
+        }
+    }
+}
